14e0b244e3d13e2dc158088c90a2adec3ae7b98d
Felipe <gaamngt@gmail.com>
2025-08-13 14:05:44 -0300

    estudando loop dentro de loop para uma matriz quadrada

---
+++ exercicios.py
@@ -0,0 +1,162 @@
+# from unittest import case
+#
+#
+# def filtrar_filmes(filmes: list, filtro: str, tipo_filtro: str) -> list:
+#     filmes_filtrados = []
+#     match tipo_filtro:
+#         case 'Ano':
+#             for filme in filmes:
+#                 if filme['Ano'] == filtro:
+#                     filmes_filtrados.append(filme)
+#
+#         case 'Genero':
+#             for filme in filmes:
+#                 if filme['Genero'] == filtro:
+#                     filmes_filtrados.append(filmes)
+#         case _:
+#             return []
+#     return filmes_filtrados
+#
+#
+# filmes = [{'nome': "carros", 'Genero': 'suspense', 'Ano': '2006'},
+#           {'nome': "carros 2", 'Genero': 'comedia', 'Ano': '2011'},
+#           {'nome': "carros 3", 'Genero': 'drama', 'Ano': '2015'}, ]
+#
+# filmes = filtrar_filmes(filmes, '2006', 'Ano')
+# print(filmes)
+#
+#
+# def votação():
+#     candidatos = [0, 0, 0]
+#     while True:
+#         voto = input('digite em quem voce quer votar para mais gay \n 1 - victor \n 2 - rapha \n 3 - felipe macho alfa \n ou sair\n')
+#         if voto == 'sair':
+#             break
+#         if voto.isdigit():
+#             voto = int(voto) - 1
+#             if 0 <= voto <= len(candidatos):
+#                 candidatos[voto] += 1
+#         else:
+#             print('resposta invalida')
+#     else:
+#         print('resposta invalida')
+#     for votos in candidatos:
+#         print(votos)
+#
+#
+# votação()
+#
+#
+#
+#
+# tarefas = [{'tarefa': 'lavar a louça' , 'prioridade': 'maxima', 'concluida': True},
+#            {'tarefa': 'comprar comida', 'prioridade': 'media', 'concluida': False}]
+#
+#
+#
+# # sair
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# opcao = 1
+# contatos = []
+# while opcao != 0:
+#     opcao = int(input("""
+#     Digite uma opção:
+#     1 - Listar Contatos.
+#     2 - Adicionar Contato.
+#     3 - Editar um Contato.
+#     4 - Excluir um contato
+#     """))
+#
+#     match opcao:
+#         case 1:
+#             for c in contatos:
+#                 print("Nome", c['nome'])
+#                 print("Telefone:", c['telefone'])
+#
+#
+#         case 2:
+#             nome = input('Digite o nome do contato que voce deseja adicionar: ')
+#             telefone = input("Digite o telefone do contato")
+#             contatos.append({'nome': nome, 'telefone': telefone})
+#
+#
+#         case 3:
+#             nome = input('Digite o nome do contato que voce deseja adicionar: ')
+#
+#             for c in contatos:
+#                 if c["nome"] == nome:
+#                     telefone = input("digite o novo telefone")
+#                     c["telefone"] = telefone
+#
+#         case 4:
+#             nome = input("digite o nomedo contato que deseja excluir")
+# #             for c in contatos:
+# #                 if c["nome"] == nome:
+# #                     contatos.remove(c)
+#
+# # matriz = []
+# # m = 3
+# # n = 3
+# #
+# # for i in range(m):
+# #     matriz.append([])
+# #     for coluna in range(n):
+# #         matriz[i].append(0)
+#
+#
+# print(matriz)
+
+# matriz = []
+# m = 3
+# n = 3
+#
+# for i in range(m):
+#     linha = []
+#     for j in range(n):
+#         linha.append(0)
+#         if i == j:
+#             linha[j] = 1
+#
+#     matriz.append(linha)
+#
+#
+# for linha in matriz:
+#     print(linha)
+#
+
+
+
+matriz = []
+m = 3
+n = 3
+
+for i in range(m):
+    linha = []
+    for j in range(n):
+        linha.append(0)
+
+        if i == j:
+
+            linha[j] = 1
+
+
+    matriz.append(linha)
+
+
+for linha in matriz:
+    print(linha)
+
+
+
+
